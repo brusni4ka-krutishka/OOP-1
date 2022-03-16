7160dfb90cd46261de223d721d0b9899eef1a003
EugeneAlWin <58369337+EugeneAlWin@users.noreply.github.com>
2022-03-16 14:35:42 +0300

    Revert "2sem started"

---
+++ Lab6/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Printer printer = new("Canon 16", "Да", 15);
+            Scanner scanner = new("Canon pixma", "Нет", 0);
+            Computer computer = new ("Hyper PC", "Да", 45);
+            Tablet tablet = new("Prestigio", "Да", 99);
+
+            Console.WriteLine("_________________________ВЫВОД ЭЛЕМЕНТОВ ЧЕРЕЗ МАССИВ________________________");
+            Console.WriteLine("-------------------------------------");
+            Mechanism[] allmech = new Mechanism[]{ printer,scanner,computer,tablet};
+            foreach (var item in allmech)
+            {
+                Printer2.IAmPrinting(item);
+                Console.WriteLine("-------------------------------------");
+            }
+            Console.WriteLine("______________________________________________________________________________");
+
+
+            Console.WriteLine($"Принтер есть принтер: {printer is Printer}");
+            Console.WriteLine($"Планшет есть планшет: {tablet is Tablet}");
+            Console.WriteLine($"Сканер есть техника: {scanner is Mechanism}\n");
+
+            computer.Clone();                   // вызов метода у класса
+            ((IAlive)computer).Clone();         // вызов метода у интерфейса
+            Console.WriteLine("______________________________________________________________________________");
+
+            Console.WriteLine("Lab6\nМетоды Контейнера: добавление, показ и удаление...");
+
+            Container ag1 = new();
+            ag1.Add();
+            ag1.Show();
+            ag1.Delete();
+
+            Console.WriteLine("\nТехника младше 3-x лет:");
+            object[] ObjectsList = new object[6];
+            ObjectsList[0] = new Test_tube(3, 10, 100);
+            ObjectsList[1] = new Test_tube(4, 12, 700);
+            ObjectsList[2] = new Test_tube(2, 9, 890);
+            ObjectsList[3] = new Test_tube(5, 13, 876);
+            ObjectsList[4] = new Test_tube(1, 123, 700);
+            ObjectsList[5] = new Test_tube(3, 10, 600);
+            byte count=0;
+            foreach (Test_tube can in ObjectsList)
+            {
+                if (can.life < 3)
+                {
+                    Console.WriteLine($"Партия колб №{count}: цена {can.cost}, кол-во {can.amount} штук.");
+                    count++;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("\nМетоды Контроллера...");
+            ControlElement a = new(false, 5, 89);
+            a.Status(); a.Switch();a.Status(); Console.WriteLine(a.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab8/CollectionType.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Lab8
+{
+    public class CollectionType<Type>:IGeneric<Type> where Type:class
+    {
+        public List<Type> passwords = new();
+
+        public void Add(Type item)
+        {
+            passwords.Add(item);
+        }
+
+        public Type Delete(int item)
+        {
+            Type value = passwords[item];
+            passwords.RemoveAt(item);
+            return value;
+        }
+
+        public void Check()
+        {
+            int count = 0;
+            foreach (Type item in passwords)
+            {
+                count++;
+                Console.WriteLine($"Элемент списка под номером {count}\nТип: {item.GetType()}\n");
+                
+            }
+        }
+        public void WriteToFile()
+        {
+            StreamWriter inFile = null;
+            try
+            {
+                inFile = new StreamWriter("..//лаба8.txt", false, Encoding.Default);
+                inFile.WriteLine("Этот текст добавлен в файл с помощью класса StreamWriter\nЛюблю записывать строки в файлы");
+            }
+            finally
+            {
+                inFile.Close();
+            }
+        }
+
+        public void ReadFromFile()
+        {
+            StreamReader fromFile = null;
+            try
+            {
+                fromFile = new StreamReader("..//лаба8.txt");
+                int counter = 0;
+
+                foreach (string str in File.ReadAllLines("..//лаба8.txt"))
+                {
+                    counter++;
+                }
+                string[] arr = new string[counter];
+                for (int i = 0; i < counter; i++)
+                {
+                    arr[i] = fromFile.ReadLine();
+                    Console.Write(arr[i]+"\n");
+                }
+            }
+            finally
+            {
+                fromFile.Close();
+            }
+        }
+    }
+    public class StandartTypes<Type1, Type2, Type3>
+    {
+        Type1 A { get; set; }
+        Type2 B { get; set; }
+        Type3 C { get; set; }
+
+        public StandartTypes(Type1 A, Type2 B, Type3 C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+        public void ShowTypes()
+        {
+            Console.WriteLine($"Первый тип {A.GetType()} со значением {A}");
+            Console.WriteLine($"Второй тип {B.GetType()} со значением {B}");
+            Console.WriteLine($"Третий тип {C.GetType()} со значением {C}");
+        }
+    }
+    public class Password // переопределение
+    {
+        private string parol;
+        public string Pass { get; set; }
+
+        public string this[string str]
+        {
+            set
+            {
+                parol = value;
+            }
+            get
+            {
+                return parol;
+            }
+
+        }
+    }
+}

8bb781490340630588f39a999403c21bf3ded6f5
Гончаревич Евгений <evgenijgoncarevic@gmail.com>
2022-04-08 13:23:30 +0300

    Temp2

--- 2sem/Lab6-7/MainWindow.xaml.cs
+++ 2sem/Lab6-7/MainWindow.xaml.cs
@@ -21,9 +21,31 @@ namespace Lab6_7
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<StockItem> ItemList = new List<StockItem>();
+
+        class StockItem
+        {
+            public string Title { get; set; }
+            public string Firm { get; set; }
+            public string Cost { get; set; }
+            public string Stock { get; set; }
+            public string ImgPath { get; set; }
+        }
+       
         public MainWindow()
         {
             InitializeComponent();
+            List<StockItem> list = new List<StockItem>
+            {
+                new StockItem{Title="Огромная пробка Red Boy", Firm="Doc Johnson", Cost="221 руб", Stock="25", ImgPath="./img/1.png"},
+                new StockItem{Title="Черный фаллоимитатор King-Sized Anal Dildo", Firm="Lovetoy", Cost="445 руб", Stock="15", ImgPath="./img/2.png"},
+                new StockItem{Title="Фаллоимитатор Мустанг Черный", Firm="Erasexa", Cost="338 руб", Stock="18", ImgPath="./img/3.png"},
+                new StockItem{Title="Лубрикант для фистинга pjur power 150 мл", Firm="Pjur", Cost="62 руб", Stock="5", ImgPath="./img/4.png"},
+                new StockItem{Title="Бондаж для рук и груди", Firm="Pjur", Cost="62 руб", Stock="5", ImgPath="./img/4.png"},
+                new StockItem{Title="Электростимулятор плуг", Firm="ОOО \"Кисс Экспо\" ", Cost="106 руб", Stock="7", ImgPath="./img/6.png"},
+            };
+            Photos.ItemsSource = list;
+
         }
 
     }
